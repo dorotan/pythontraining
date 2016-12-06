d946767e107408b8707c79591d15f3f51833f37d
noro86 <niezborala.dorota@gmail.com>
2016-12-06 22:00:21 +0100

    fixture before each test, refine auxiliary class, login and logout, check the current status of the session

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,6 +1,5 @@
 __author__ = 'dorota'
 
+
 def test_delete_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.delete_first_contact()
-    app.session.logout()
