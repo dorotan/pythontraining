d2e442089f3ee5e9d0b1a9f6530ca8b664c3d90b
noro86 <niezborala.dorota@gmail.com>
2016-12-18 19:58:32 +0100

    Optimized method

--- fixture/application.py
+++ fixture/application.py
@@ -14,20 +14,14 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-        self.open_home_page()
+        self.open_homepage()
 
-    def is_valid(self):
-        try:
-            self.wd.current_url
-            return True
-        except:
-            return False
-
-    def open_home_page(self):
-        wd = self.wd
-        if not (wd.current_url.endswith("index.php") and len(wd.find_elements_by_link_text("CREATE_ACCOUNT")) > 0):
-            wd.get("http://localhost:81/addressbook")
-        pass
+    def open_homepage(self):
+        def open_home_page(self):
+            wd = self.wd
+            if not (wd.current_url.endswith("index.php") and len(wd.find_elements_by_link_text("CREATE_ACCOUNT")) > 0):
+                wd.get("http://localhost:81/addressbook")
+            pass
 
     def destroy(self):
         self.wd.quit()
