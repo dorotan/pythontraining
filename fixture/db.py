0934218b15e0d0752cf7f5c5eff920d7b7dfe576
noro86 <niezborala.dorota@gmail.com>
2017-01-18 22:56:49 +0100

    changes II

--- fixture/db.py
+++ fixture/db.py
@@ -28,16 +28,14 @@ class DbFixture:
             cursor.close()
         return list
 
-
     def get_contact_list(self):
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, nickname, company, title, address, fax, notes from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, nickname, firstname, lastname, company, title, address, fax, notes) = row
-                list.append(Contact(id=id, nickname=nickname, firstname=firstname, lastname=lastname, company=company,
-                                    title=title, address=address, fax=fax, notes=notes))
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
         finally:
             cursor.close()
         return list
